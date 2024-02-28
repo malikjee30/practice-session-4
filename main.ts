2e52cad723aa9eba864f952a05d1ac6d8418504e
malikjee30 <ghulamnasir30@gmail.com>
2024-02-29 01:28:44 +0500

    initial files

---
+++ main.ts
@@ -0,0 +1,72 @@
+//string data type:
+// console.log("123")
+// console.log('ahmad')
+// console.log(`nasir`)
+
+//number data type:
+// console.log(10)
+// console.log(100)
+// console.log(1000)
+// console.log(10000)
+
+//to check type of any data "typeof"
+// console.log(typeof '100');
+// console.log(typeof 100);
+
+//boolean data type
+// console.log(true);
+// console.log(false);
+// console.log(5===5);
+// console.log(5===3);
+
+// console.log('hello', 'world', 'this is the typescript session');
+// console.log( 'hello'+'world'+'alart', 'this the typescript session');concatenation
+
+//variables
+//variables declaration: var, let, const
+
+//let box1 = 'apple';
+// const  container = 'ball';
+// var myVariable = 500;
+// console.log(box1)
+// console.log(container)
+// console.log(myVariable)
+// console.log(typeof container)
+// console.log(typeof myVariable)
+
+
+//variable type annotations
+
+// 
+//variable naming convention
+
+// let thereAreTwoBox = "offlowers"; //camel case
+// let there_are_two_box = "offlowers"; // snake case
+// let ThereAreTwoBox = "offlowers";  //pascal case
+
+
+//updation
+
+// var nasirBox = 'scewrench'  //declaration
+//   nasirBox = 'screwdriver' //updation
+//   var nasirBox  = 'pipewrench'  //redeclation
+
+//   let nasir2Box = 'laptop'  //declaration
+//   nasir2Box = 'tablet' //updation
+//   let nasir2Box = 'mobile'  //redeclaration (notpossible)
+
+//   const nasir3Box = 'mobile' //declare
+//   nasir3Box = 'charger' //update (notpossible)
+//   const nasir3Box = //redeclaration (notpossible)
+
+
+// let x = 15;
+// if ( x===15){
+//     let mybox = 'flower';     //block scope
+//     const hisbox = 'redrose';   // block scope
+//     var yourbox =  'pinkrose';  // global scope
+//     console.log(hisbox)
+// }
+//console.log(hisbox)
+
+
